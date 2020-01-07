f1ab1b6dd2c19b2045ebaa3102a1849e54fb5e10
elio-tsui <elio1900@dingtalk.com>
2020-01-07 15:35:58 +0800

    modify

--- recp.sh
+++ recp.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 os=`uname -s`
 
-echo $*
 
 for i in $*;
 do
     case ${i##*.} in 
-    
+        
         php | sh)
             suffix="#"
         ;;
 
-        * )
+        java | js | cpp | py | tmp)
             suffix="//"
         ;;
 
+        * )
+            echo "the file name is illegal"
+            exit 1
     esac
     
     echo "$suffix $i";
     cat $i;
-    echo;
+    echo "hello";
 
 done > /tmp/storageInfo 
 
@@ -31,18 +33,14 @@ case $os in
         cat /tmp/storageInfo | xclip -sel clip
         echo "success1"
     ;;
-
+    
     #MacOS
     Darwin )
         cat /tmp/storageInfo | pbcopy
-        echo "success"
+        echo "success2"
     ;;
 
     * )
         echo -e "OS '$os' not supported yet !\n"
-        exit 2
+        exit 1
 esac
-
-
-
-
